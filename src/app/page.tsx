618653b64ba28095a7b4e5bedf0e634d824cefee
Ari Quinones <ariquinones0015@gmail.com>
2024-03-13 12:56:48 -0400

    deploying first time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,8 +23,10 @@ async function getContent(searchParams: SearchParams) {
     client?: Awaited<ReturnType<typeof copilot.retrieveAClient>>;
     company?: Awaited<ReturnType<typeof copilot.retrieveACompany>>;
     internalUser?: Awaited<ReturnType<typeof copilot.retrieveAnInternalUser>>;
+    channels?: Awaited<ReturnType<typeof copilot.listFileChannels>>;
   } = {
     workspace: await copilot.getWorkspaceInfo(),
+    channels: await copilot.listFileChannels({}),
   };
   const tokenPayload = await copilot.getTokenPayload?.();
 
@@ -41,8 +43,6 @@ async function getContent(searchParams: SearchParams) {
       id: tokenPayload.internalUserId,
     });
   }
-
-  // TODO add data.workspace here.
   return data;
 }
 
@@ -52,15 +52,19 @@ export default async function Page({
   searchParams: SearchParams;
 }) {
   const data = await getContent(searchParams);
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Welcome to the custom app&nbsp;
-          <code className="font-mono font-bold">
-            {data.client ? data.client.givenName : data.company?.name}
+          Welcome to the custom app for file channel sharing 
+          <code className="font-mono font-bold pl-2">
+            {/* {data.client ? data.client.givenName : data.company?.name} */}
+            { data.workspace ? `   ${data.workspace.brandName}` : " nothing"}
           </code>
-          ,
+        </p>
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+            { data.channels ? 'You have ' + data.channels.data.length + ' file channels to query from.' : ''}
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -82,7 +86,7 @@ export default async function Page({
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/copilot_logo.png"
@@ -161,7 +165,7 @@ export default async function Page({
             Read the details in the repo docs to learn how to deploy.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
   );
 }
